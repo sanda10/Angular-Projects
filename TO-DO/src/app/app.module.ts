1566f4ac12842bcf12d77cbe05f038288a9f212d
sanda10 <119898147+sanda10@users.noreply.github.com>
2025-05-23 12:21:29 +0300

    Add files via upload

---
+++ TO-DO/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { TodolistComponent } from './todolist/todolist.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodolistComponent
+  ],
+  imports: [
+    FormsModule,
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
